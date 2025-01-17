8b4cc0b9930658dd28ab6ef31d49c939ff8cd41e
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-01-17 16:23:30 +0530

    implement a feature that will show default image for an empty array returned by giphy

--- app.js
+++ app.js
@@ -2,51 +2,55 @@ const img = document.querySelector(".gif-image");
 const searchIcon = document.querySelector("#search-icon");
 let searchInput = document.querySelector("#search-input");
 
-
 function useFetchApi(url) {
+  fetch(url, { mode: "cors" })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
 
-  fetch(url,{ mode: "cors" }).then((response)=>{
-      return response.json();}
-  ).then((response) => {
-    img.src = response.data.images.original.url;
-  }).catch((err)=>{
-      img.src = "./images/cat-gif.gif"
-  });
-  }
-
-  //
+      if (response.data && Object.keys(response.data).length !== 0) {
+        img.src = response.data.images.original.url;
+      } else {
+        img.src = "./images/cat-gif.gif";
+      }
+    })
+    .catch((err) => {
+      img.src = "./images/cat-gif.gif";
+    });
+}
 
 // initialize the app
-useFetchApi("https://api.giphy.com/v1/gifs/random?api_key=MUzjBZUJN1WQ7PnlpYY6uH4OLET8rAJL&tag=cats");
+useFetchApi(
+  "https://api.giphy.com/v1/gifs/random?api_key=MUzjBZUJN1WQ7PnlpYY6uH4OLET8rAJL&tag=cats"
+);
 
-
-//searchicon event listener//
-searchIcon.addEventListener("click",()=>{
-  
-  let baseUrl = "https://api.giphy.com/v1/gifs/random?api_key=MUzjBZUJN1WQ7PnlpYY6uH4OLET8rAJL&tag=";
+//search icon event listener//
+searchIcon.addEventListener("click", () => {
+  let baseUrl =
+    "https://api.giphy.com/v1/gifs/random?api_key=MUzjBZUJN1WQ7PnlpYY6uH4OLET8rAJL&tag=";
   let tagQuery = searchInput.value.trim();
   let url = baseUrl + tagQuery;
-  useFetchApi(url); 
+  useFetchApi(url);
 
   //clear the urls
   baseUrl = "";
   tagQuery = "";
   url = "";
-  
-})
-
+});
 
 searchInput.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    let baseUrl = "https://api.giphy.com/v1/gifs/random?api_key=MUzjBZUJN1WQ7PnlpYY6uH4OLET8rAJL&tag=";
+    let baseUrl =
+      "https://api.giphy.com/v1/gifs/random?api_key=MUzjBZUJN1WQ7PnlpYY6uH4OLET8rAJL&tag=";
     let tagQuery = searchInput.value.trim();
     let url = baseUrl + tagQuery;
-    useFetchApi(url); 
-  
+    useFetchApi(url);
+
     //clear the urls
     baseUrl = "";
     tagQuery = "";
     url = "";
-     
   }
 });
